09b104906c9b184060a6e54b54b13b589d10d4fd
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-04-26 22:06:14 +0900

    Add files via upload

---
+++ [JS 강진하]/수업/0426/JS/main.js
@@ -0,0 +1,16 @@
+//$("선택할 태그의 태그명이나, 클래스명, 아이디명-css 선택자 방식")
+//$("*").css("font-size","13px");
+  $(document).ready(function(){
+      $("*").css("font-size","13px");
+      S("h1").css("color","red");
+      $("#firstTitle").css("color","green");
+      $(".nextTitle").css("color","blue");
+      $(".obj1").parent():css("background","orange");
+      $(".obj3 > li").css("font-weight","bold");
+      $(".obj3").children(".theObj1").css("color","purple");
+      $(".obj4").prev().css("border","1px solid red");
+      $(".obj5").next().css("border"."1px solid blue").css("background","orage");//제이쿼리 체이닝 기법
+      $(".obj6").siblings().css({
+      "text-align":"center",
+      "color":"red"//선택 대상 제외 나머지 형제들을 리턴
+  });
