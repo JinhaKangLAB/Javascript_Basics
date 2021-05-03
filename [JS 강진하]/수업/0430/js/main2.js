727fb5f665736bbdd8831e2df9c2205940197153
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-05-03 19:58:48 +0900

    Add files via upload

--- [JS 강진하]/수업/0430/js/main2.js
+++ [JS 강진하]/수업/0430/js/main2.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
  $(".tabMenu1 > dt").click(function(){
   $(".tabMenu1 > dt").removeClass("on");
-   $(this).addClass("on");
+   $(this).addClass("on"); //사용자 클릭 dt
    $(".tabMenu1 > dd").removeClass("on");
    $(this).next().addClass("on");
  });
@@ -15,4 +15,16 @@ $(".tab li").click(function(){
   $(".des > p").removeClass("on");
   $(".des > p").eq(a).addClass("on");
 });
+var des3List = ['Lorem1 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati',
+'Lorem2 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ',
+'Lorem3 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru'
+];
+
+$(".des3").text(des3List[0]);
+$("tab3 li").click(function(){
+  $(".tab3 li").removeClass("on");
+    $(this).addClass("on");
+
+})
+
 });
